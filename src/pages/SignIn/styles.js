a78af8901d2bf1570fd07f0adc1fbc2a90d82578
arauujo <wesleey988@gmail.com>
2024-11-20 21:33:36 -0300

    refactor SignIn styles for improved layout and responsiveness

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
-  height: 100vh;
-  margin-top: -22%;
+  margin-top: -6rem;
+  width: 100vw;
   padding: 0 3.91rem 0 5.41rem;
   display: flex;
+  gap: 6.1rem;
   flex-direction: column;
-  justify-content: center;
 
   > .logo-container {
-    margin: 0 auto;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -21,15 +21,54 @@ export const Container = styled.div`
     }
 
     h1 {
+      font-family: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontFamily};
       font-size: 3.1rem;
+      font-weight: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontWeight};
+      line-height: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.lineHeight};
+    }
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
+    margin-top: -3rem;
+    padding: 0 6.75rem 0 9.62rem;
+    flex-direction: row;
+    justify-content: space-between;
+
+    > .logo-container {
+      margin-top: 0;
+
+      h1 {
+        font-size: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontSize};
+      }
     }
   }
 `;
 
 export const Form = styled.form`
-  margin-top: 6.1rem;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   gap: 32px;
+
+  .desktop-only {
+    display: none;
+  }
+
+  > h2 {
+    font-family: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM.fontFamily};
+    font-size: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM.fontSize};
+    font-weight: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM.fontWeight};
+    line-height: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM.lineHeight};
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
+    background-color: ${({ theme }) => theme.COLORS.DARK_700};
+    padding: 4rem;
+    border-radius: 1rem;
+    width: 29.75rem;
+
+    .desktop-only {
+      display: block;
+      text-align: center;
+    }
+  }
 `;
