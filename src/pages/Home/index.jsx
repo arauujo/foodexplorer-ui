458dd0c6478d015151cd974c4616197f0d9e84c1
arauujo <wesleey988@gmail.com>
2024-11-30 18:10:07 -0300

    Add Home page with Header and SideMenu components for navigation

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import { SideMenu } from "../../components/SideMenu";
+import { Container } from "./styles";
+
+export function Home() {
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setIsSideMenuOpen(true);
+  };
+
+  const handleCloseMenu = () => {
+    setIsSideMenuOpen(false);
+  };
+
+  return (
+    <Container>
+      <Header onOpenMenu={handleOpenMenu} />
+      <SideMenu isOpen={isSideMenuOpen} onClose={handleCloseMenu} />
+    </Container>
+  );
+}
