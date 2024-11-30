2853a556b4c3df6638624b6ded8dcd4a452dc790
arauujo <wesleey988@gmail.com>
2024-11-30 20:06:21 -0300

    Add Footer component to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Header } from "../../components/Header";
 import { SideMenu } from "../../components/SideMenu";
+import { Footer } from "../../components/Footer";
 import { Container } from "./styles";
 
 export function Home() {
@@ -18,6 +19,7 @@ export function Home() {
     <Container>
       <Header onOpenMenu={handleOpenMenu} />
       <SideMenu isOpen={isSideMenuOpen} onClose={handleCloseMenu} />
+      <Footer />
     </Container>
   );
 }
