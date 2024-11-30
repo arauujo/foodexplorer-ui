9f876890acb11d56fc2b5f372b7c3dcbce363db1
arauujo <wesleey988@gmail.com>
2024-11-30 20:35:46 -0300

    Add Logo component

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,18 @@
+export function Logo() {
+  return (
+    <svg
+      width="26"
+      height="31"
+      viewBox="0 0 26 31"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="polygon-logo"
+    >
+      <path
+        d="M0.509619 8.15507L13 0.943744L25.4904 8.15507V22.5777L13 29.789L0.509619 22.5777V8.15507Z"
+        fill="#065E7C"
+        stroke="black"
+      />
+    </svg>
+  );
+}
