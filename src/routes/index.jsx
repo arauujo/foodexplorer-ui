e45b34cd3eb9d054dd48c3d6003214f6f951c19a
arauujo <wesleey988@gmail.com>
2024-11-30 00:09:20 -0300

    Add routing for home page and conditional rendering based on authentication status

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
+import { useAuth } from "../hooks/auth";
 
 export function Routes() {
+  const isAuthenticated = useAuth().user;
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {isAuthenticated ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
