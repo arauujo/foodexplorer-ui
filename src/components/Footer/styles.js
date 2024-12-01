79560d83b15bf6dea4fcd2e3e0124c01532f7d4d
arauujo <wesleey988@gmail.com>
2024-12-01 19:17:56 -0300

    Change Footer position from absolute to relative for better layout control

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
-  position: absolute;
+  position: relative;
   bottom: 0;
   left: 0;
   width: 100%;
