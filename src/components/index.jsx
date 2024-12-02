bb63568d59587bdbd8a2529f7c9211ae0ae70a41
arauujo <wesleey988@gmail.com>
2024-12-01 21:29:19 -0300

    Add index file to export components for easier imports

---
+++ src/components/index.jsx
@@ -0,0 +1,21 @@
+import { Button } from "./Button";
+import { ButtonText } from "./ButtonText";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Input } from "./Input";
+import { Label } from "./Label";
+import { Logo } from "./Logo";
+import { MealCard } from "./MealCard";
+import { SideMenu } from "./SideMenu";
+
+export {
+  Button,
+  ButtonText,
+  Footer,
+  Header,
+  Input,
+  Label,
+  Logo,
+  MealCard,
+  SideMenu,
+};
