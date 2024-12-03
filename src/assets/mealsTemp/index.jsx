535d1ac00ea004d831359df04354633d720d978c
arauujo <wesleey988@gmail.com>
2024-12-03 00:31:17 -0300

    Add meal images for example

---
+++ src/assets/mealsTemp/index.jsx
@@ -0,0 +1,27 @@
+import boloDamasco from "./bolo-de-damasco.png";
+import espresso from "./espresso.png";
+import macarons from "./macarons.png";
+import peachyPastries from "./peachy-pastrie.png";
+import pomoBourbon from "./pomo-bourbon.png";
+import prugnaPie from "./prugna-pie.png";
+import saladaMolla from "./salada-molla.png";
+import saladaRavanello from "./salada-ravanello.png";
+import spaguettiGambe from "./spaguetti-gambe.png";
+import sucoMaracuja from "./suco-de-maracuja.png";
+import teDautunno from "./te-dautunno.png";
+import torradasParma from "./torradas-de-parma.png";
+
+export {
+  boloDamasco,
+  espresso,
+  macarons,
+  peachyPastries,
+  pomoBourbon,
+  prugnaPie,
+  saladaMolla,
+  saladaRavanello,
+  spaguettiGambe,
+  sucoMaracuja,
+  teDautunno,
+  torradasParma,
+};
